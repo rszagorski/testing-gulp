f85271ea0f010d1865466da99929a04bdaced0e4
Remy Zagorski <rszagorski@gmail.com>
2016-10-25 12:49:10 -0400

    initial commit

---
+++ dev/scripts/main.js
@@ -0,0 +1,7 @@
+const ohHey = "Hello World";
+
+const numbers = [1,2,3,4];
+
+const maxNum = Math.max(...numbers);
+
+console.log(ohHey);
---
+++ public/scripts/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var ohHey = "Hello World";
+
+var numbers = [1, 2, 3, 4];
+
+var maxNum = Math.max.apply(Math, numbers);
+
+console.log(ohHey);
